327adfc34ecf527eb4910bc3e602118aa8560808
m2vi <m2vi>
2021-07-03 22:43:49 +0200

    init

--- projects/downloader/components/youtube/Page.tsx
+++ projects/downloader/components/youtube/Page.tsx
@@ -1,16 +1,15 @@
-import { Bar, BarButton } from './Bar';
-import { IoSearch, IoInformationCircleOutline } from 'react-icons/io5';
-import React, { useEffect, useState } from 'react';
-import Tooltip from '../Tooltip';
-import { useTranslation } from 'next-i18next';
-import keyboardKey, { Play, t } from 'keyboard-key';
-import { validateURL } from 'ytdl-core';
-import { Button } from '../Button';
-import { genDownloadLink, parsedProps } from '../../utils/youtube/youtube';
-import Player from './Player';
-import Modal from './Modal';
-import { useModal } from '../../context/Modal';
-import { getSupportedItags } from '../../utils/youtube/itags';
+import { Bar, BarButton } from "./Bar";
+import { IoSearch, IoInformationCircleOutline } from "react-icons/io5";
+import React, { useEffect, useState } from "react";
+import Tooltip from "../Tooltip";
+import { useTranslation } from "next-i18next";
+import keyboardKey, { Play, t } from "keyboard-key";
+import { validateURL } from "ytdl-core";
+import { Button } from "../Button";
+import { genDownloadLink, parsedProps } from "../../utils/youtube/youtube";
+import Player from "./Player";
+import Modal from "./Modal";
+import { useModal } from "../../context/Modal";
 
 export const Page = () => {
   const { t } = useTranslation();
@@ -38,39 +37,22 @@ export const Page = () => {
     }
 
     const req = `/api/youtube/info?v=${input}`;
-    const res = await fetch(req, { method: 'GET' });
+    const res = await fetch(req, { method: "GET" });
     const json = await res.json();
     setInfo(json);
   };
 
   return (
     <>
-      <div className='w-full flex flex-col items-center'>
-        <div
-          className='max-w-3xl mt-10 flex flex-row items-center w-full'
-          style={{ height: '40px' }}
-        >
-          <BarButton className='rounded-r-0' onClick={() => loadVideo()}>
-            <IoSearch className='h-4 w-4 text-primary-300' />
+      <div className="w-full flex flex-col items-center">
+        <div className="max-w-3xl mt-10 flex flex-row items-center w-full" style={{ height: "40px" }}>
+          <BarButton className="rounded-r-0" onClick={() => loadVideo()}>
+            <IoSearch className="h-4 w-4 text-primary-300" />
           </BarButton>
-          <Bar
-            placeholder='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-            onKeyUp={handleKeyUp}
-            ref={barRef}
-          />
-          <BarButton
-            className='rounded-l-0'
-            onClick={() =>
-              location.replace(
-                'https://github.com/m2vi/downloader/blob/main/README.md'
-              )
-            }
-          >
+          <Bar placeholder="https://www.youtube.com/watch?v=dQw4w9WgXcQ" onKeyUp={handleKeyUp} ref={barRef} />
+          <BarButton className="rounded-l-0" onClick={() => location.replace("https://github.com/m2vi/downloader/blob/main/README.md")}>
             <Tooltip>
-              <IoInformationCircleOutline
-                data-tip={t('pages.documentation')}
-                className='h-4 w-4 text-primary-300'
-              />
+              <IoInformationCircleOutline data-tip={t("pages.documentation")} className="h-4 w-4 text-primary-300" />
             </Tooltip>
           </BarButton>
         </div>
@@ -89,31 +71,27 @@ export const Main = ({ info }: MainProps) => {
   const { thumbnails, title, video_url } = info;
   const playerWrapperRef = React.createRef<HTMLDivElement>();
 
-  useEffect(() => console.log(info), []);
+  useEffect(() => console.log(info), [info]);
   const {
     state: { isOpen },
     dispatch,
   } = useModal();
   return (
     <>
-      <div className='w-full mt-9 px-9 max-w-3xl flex flex-col items-center'>
+      <div className="w-full mt-9 px-9 max-w-3xl flex flex-col items-center">
         <Player info={info} />
 
-        <div className='pt-7 flex justify-center items-center'>
+        <div className="pt-7 flex justify-center items-center">
           <Button
-            className='mr-1'
+            className="mr-1"
             onClick={() => {
               console.log(info.itags);
             }}
           >
-            {t('actions.download')}
+            {t("actions.download")}
           </Button>
-          <Button
-            className='ml-1'
-            color={'secondary'}
-            onClick={() => dispatch({ type: 'setOpen', value: true })}
-          >
-            {t('actions.configure')}
+          <Button className="ml-1" color={"secondary"} onClick={() => dispatch({ type: "setOpen", value: true })}>
+            {t("actions.configure")}
           </Button>
         </div>
       </div>
@@ -123,7 +101,7 @@ export const Main = ({ info }: MainProps) => {
 };
 
 export const MainHolder = ({ info }: MainProps) => {
-  const valid = info !== undefined || typeof info !== 'undefined';
+  const valid = info !== undefined || typeof info !== "undefined";
 
   interface BinProps {
     error: boolean;
@@ -131,18 +109,13 @@ export const MainHolder = ({ info }: MainProps) => {
 
   const Bin = ({ error }: BinProps) => {
     return (
-      <div className='w-full mt-9 px-9 max-w-3xl flex flex-col items-center'>
-        <div
-          className='preview bg-primary-800 h-full w-full rounded-8 cursor-pointer'
-          style={{ aspectRatio: '16 / 9' }}
-        ></div>
+      <div className="w-full mt-9 px-9 max-w-3xl flex flex-col items-center">
+        <div className="preview bg-primary-800 h-full w-full rounded-8 cursor-pointer" style={{ aspectRatio: "16 / 9" }}></div>
       </div>
     );
   };
 
-  return (
-    <>{!valid || !info.success ? <Bin error={true} /> : <Main info={info} />}</>
-  );
+  return <>{!valid || !info.success ? <Bin error={true} /> : <Main info={info} />}</>;
 };
 
 export default Page;
