30218a209d4886730a38de54fbc75c5556eceda4
m2v <69117810+iuvoJS@users.noreply.github.com>
2021-08-23 22:46:19 +0200

    yuki

--- components/pages/hub/Timers.tsx
+++ components/pages/hub/Timers.tsx
@@ -7,24 +7,24 @@ const Timers = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="h-full grid gap-6 grid-rows-2" style={{ width: 'calc(905px - 504px - 30px)' }}>
+    <div className="h-full grid" style={{ width: 'calc(905px - 504px - 30px)' }}>
       <div className="grid grid-rows-2 grid-cols-3 w-full rounded-8 gap-6">
         {sortByKey(user.timers, 'date').map(({ icon: Icon, date, name }) => {
-          const time = moment(date).fromNow(true);
+          const time = [moment(date).fromNow(true), `${moment(date).diff(this, 'days').toString()} days`];
 
           return (
             <div
               className="bg-primary-800 h-widget rounded-8 text-primary-200 cursor-pointer text-center py-4 flex flex-col items-center border-primary-800"
               key={name}
+              title={time[1]}
             >
               <Icon className="h-4 w-4 mb-2" />
               <p className="small font-semibold text-primary-100 mb-1">{t(`pages.hub.timer.${name}`)}</p>
-              <p className="reallySmall">{time}</p>
+              <p className="reallySmall">{time[0]}</p>
             </div>
           );
         })}
       </div>
-      <div className="flex flex-col w-full bg-primary-800 p-4 rounded-8"></div>
     </div>
   );
 };
