65a571e0e4c3439c576965304f6c0ddbc0a3c8a7
m2v <69117810+iuvoJS@users.noreply.github.com>
2021-09-12 09:05:17 +0200

    bones

--- pages/api/ip.ts
+++ pages/api/ip.ts
@@ -1,9 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import requestIp from 'request-ip';
+import ipp from 'ip';
 
 const ip = async (_: NextApiRequest, res: NextApiResponse) => {
   try {
-    res.status(200).json({ ip: requestIp.getClientIp(_) });
+    res.status(200).json({ ip: requestIp.getClientIp(_), ip2: ipp.address() });
   } catch (error) {}
 };
 
